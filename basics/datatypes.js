259d4933f3bc2225ca5bd511dc95564b8783e2a6
DEVA-1992 <146052797+DEVA-1992@users.noreply.github.com>
2023-09-27 21:53:47 +0000

    value_assign

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,4 +1,11 @@
-let number;
-let name;
+let num;
+let nam;
 let option;
 let obj;
+let symb;
+num=12;//Number
+nam="Deva";//string
+option=true;//boolean
+obj={firstName:"nand", lastName:"Deva"};//object
+const cars=["bmw","suzuki"];//array
+console.log(num,nam,option,obj,cars);
