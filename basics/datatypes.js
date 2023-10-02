3cd0e2a281680a0f4fd61a34cb2a0c530e089fa2
DEVA-1992 <146052797+DEVA-1992@users.noreply.github.com>
2023-10-02 20:27:00 +0000

    different type of data

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -8,4 +8,19 @@ nam="Deva";//string
 option=true;//boolean
 obj={firstName:"nand", lastName:"Deva"};//object
 const cars=["bmw","suzuki"];//array
-console.log(num,nam,option,obj,cars);
+console.log(num,nam,option,obj,cars,typeof(option));// showing num value, nam, 
+
+
+
+const x=11;
+const y="11";
+const ifxy=true
+let sum;
+sum=x+y;
+console.log(sum);
+sum=x+Number(y);//typecasting string to number
+const z=null;// null value
+let c;
+console.log(sum);
+console.log(typeof(z),typeof(c));// z is a object and c is undefined
+
